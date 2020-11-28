1ebcd8ce7162994c8b74aabdb3549b23867488a5
Hamza Rashid <hamzarashid592@yahoo.com>
2020-11-28 20:52:31 +0500

    Added custom toast and layout inflater project

--- AndroidToast/app/src/main/java/com/example/androidtoast/MainActivity.java
+++ AndroidToast/app/src/main/java/com/example/androidtoast/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.androidtoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1,btn2;
+    Button btn1,btn2,btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         btn1=(Button) findViewById(R.id.btn1);
         btn2=(Button) findViewById(R.id.btn2);
+        btn3=(Button) findViewById(R.id.btn3);
 
         btn1.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -34,5 +39,21 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"This is Short",Toast.LENGTH_SHORT).show();
             }
         });
+
+        btn3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                LayoutInflater li= getLayoutInflater(); //This class fetches views from another layout.
+                View layout=  li.inflate(R.layout.activity_custom_toast, (ViewGroup) findViewById(R.id.customToast)); //This method does the actual fetching. We pass the layout to be fetched here.
+
+                Toast toast= new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER,0,0);
+                toast.setView(layout);
+                toast.show();
+            }
+        });
+
+
     }
 }
