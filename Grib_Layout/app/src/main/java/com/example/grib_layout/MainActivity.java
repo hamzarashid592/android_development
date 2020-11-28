072617b8edd77b9d11ec82e77c8a4de544aa59d0
Hamza Rashid <hamzarashid592@yahoo.com>
2020-11-28 18:13:05 +0500

    Added grid layout project

---
+++ Grib_Layout/app/src/main/java/com/example/grib_layout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.grib_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
