abfeba66e281d0cd9a5f9ad11ddf2ae7c88f91b9
Hamza Rashid <hamzarashid592@yahoo.com>
2020-11-28 21:36:29 +0500

    Added toggle button in the toast project

--- AndroidToast/app/src/main/java/com/example/androidtoast/MainActivity.java
+++ AndroidToast/app/src/main/java/com/example/androidtoast/MainActivity.java
@@ -10,10 +10,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1,btn2,btn3;
+    Button btn1,btn2,btn3,toggleSubmit;
+    ToggleButton toggle1, toggle2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
         btn1=(Button) findViewById(R.id.btn1);
         btn2=(Button) findViewById(R.id.btn2);
         btn3=(Button) findViewById(R.id.btn3);
+        toggleSubmit=(Button)findViewById(R.id.toggleResult);
+        toggle1=(ToggleButton)findViewById(R.id.toggleButton1);
+        toggle2=(ToggleButton)findViewById(R.id.toggleButton2);
 
         btn1.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -54,6 +59,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        toggleSubmit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                StringBuilder str_builder= new StringBuilder();
+                str_builder.append("Radio State: ").append(toggle1.getText());
+                str_builder.append("\nTV State: ").append(toggle2.getText());
+
+
+                Toast toast= new Toast(getApplicationContext());
+                toast.setText(str_builder.toString());
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        toggle1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                StringBuilder str_builder= new StringBuilder();
+                str_builder.append("Radio State: ").append(toggle1.getText());
+                Toast toast= new Toast(getApplicationContext());
+                toast.setText(str_builder.toString());
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        toggle2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                StringBuilder str_builder= new StringBuilder();
+                str_builder.append("TV State: ").append(toggle2.getText());
+                Toast toast= new Toast(getApplicationContext());
+                toast.setText(str_builder.toString());
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
 
     }
 }
