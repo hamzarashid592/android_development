08f1d528d1359ca480bfff91a9b7b63337cf1561
Hamza Rashid <hamzarashid592@yahoo.com>
2020-12-05 14:02:38 +0500

    Optimized array adpater program

--- Playing_with_Adapters_2/app/src/main/java/com/example/playing_with_adapters_2/array_adapter_3.java
+++ Playing_with_Adapters_2/app/src/main/java/com/example/playing_with_adapters_2/array_adapter_3.java
@@ -8,6 +8,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +22,8 @@ import java.util.List;
 public class array_adapter_3 extends AppCompatActivity {
 
     String titles[], description[];
-    Integer images[]={R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e};
+    Integer images[]={R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,
+            R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e};
     ListView listView1;
 
     @Override
@@ -60,17 +62,50 @@ class CustomArrayAdapter extends ArrayAdapter<String>{
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        LayoutInflater li= (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View row=li.inflate(R.layout.row_layout_2,parent,false);
 
-        TextView titleTextView=row.findViewById(R.id.titleTextView);
-        TextView descTextView=row.findViewById(R.id.descTestView);
-        ImageView imageView=row.findViewById(R.id.imageView);
+//        Regular way of doing it....
+//        LayoutInflater li= (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        View row=li.inflate(R.layout.row_layout_2,parent,false);
+//
+//        TextView titleTextView=row.findViewById(R.id.titleTextView);
+//        TextView descTextView=row.findViewById(R.id.descTestView);
+//        ImageView imageView=row.findViewById(R.id.imageView);
+//
+//        titleTextView.setText(titles[position]);
+//        descTextView.setText(desc[position]);
+//        imageView.setImageResource(images[position]);
 
-        titleTextView.setText(titles[position]);
-        descTextView.setText(desc[position]);
-        imageView.setImageResource(images[position]);
+//        Optimized way of doing it.
+        View row=convertView;
+        MyViewHolder holder=null;
+        if (row==null){
+            LayoutInflater li= (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row=li.inflate(R.layout.row_layout_2,parent,false);
+            holder= new MyViewHolder(row);
+            row.setTag(holder);
+            Log.d("HAMZA","Creating new views at position "+position);
+        }
+        else {
+            Log.d("Hamza", "Recycling old views");
+            holder = (MyViewHolder) row.getTag();
+        }
+
+        holder.titleTextView.setText(titles[position]);
+        holder.descTextView.setText(desc[position]);
+        holder.imageView.setImageResource(images[position]);
 
         return row;
     }
+
+    class MyViewHolder{
+        TextView titleTextView;
+        TextView descTextView;
+        ImageView imageView;
+
+        public MyViewHolder(View v){
+            titleTextView=v.findViewById(R.id.titleTextView);
+            descTextView=v.findViewById(R.id.descTestView);
+            imageView=v.findViewById(R.id.imageView);
+        }
+    };
 }
